6308805a9bbfd6a656ee981dff4a2c1d9a2b11fe
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-08-01 22:30:18 +0700

    Delete factory class

--- core/src/main/java/com/jonrysimbolon/core/base/BaseViewModel.kt
+++ core/src/main/java/com/jonrysimbolon/core/base/BaseViewModel.kt
@@ -3,7 +3,6 @@ package com.jonrysimbolon.core.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavDirections
 import com.jonrysimbolon.core.navigation.NavigationCommand
 import com.jonrysimbolon.core.utils.Event
@@ -18,11 +17,5 @@ abstract class BaseViewModel: ViewModel() {
 
      fun navigate(directions: NavDirections) {
         _navigation.value = Event(NavigationCommand.To(directions))
-    }
-}
-
-class BaseViewModelFactory : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return modelClass.getConstructor().newInstance()
     }
 }
