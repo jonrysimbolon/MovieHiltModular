06534eea52e65f8d46ac2e085f2d991c1ddc89b6
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-08-01 19:36:59 +0700

    Remove command

--- core/src/main/java/com/jonrysimbolon/core/utils/Event.kt
+++ core/src/main/java/com/jonrysimbolon/core/utils/Event.kt
@@ -1,16 +1,9 @@
-package com.stockbit.common.utils
+package com.jonrysimbolon.core.utils
 
-/**
- * Used as a wrapper for data that is exposed via a LiveData that represents an event.
- * @see https://medium.com/androiddevelopers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150
- */
 open class Event<out T>(private val content: T) {
 
     private var hasBeenHandled = false
 
-    /**
-     * Returns the content and prevents its use again.
-     */
     fun getContentIfNotHandled(): T? {
         return if (hasBeenHandled) {
             null
@@ -19,9 +12,6 @@ open class Event<out T>(private val content: T) {
             content
         }
     }
-
-    /**
-     * Returns the content, even if it's already been handled.
-     */
+    
     fun peekContent(): T = content
 }
