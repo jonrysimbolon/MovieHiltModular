31e1ee4d08481a2258bad1061fe63b4a50447a0b
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-08-25 16:43:09 +0700

    docs: empty detail kotlin and make adjustment on detail xml with pin toolbar and move imageview on detail content scrolling

--- app/src/main/java/com/jonrysimbolon/moviehiltmodular/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/jonrysimbolon/moviehiltmodular/presentation/detail/DetailFragment.kt
@@ -1,8 +1,12 @@
 package com.jonrysimbolon.moviehiltmodular.presentation.detail
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.jonrysimbolon.core.base.BaseFragment
+import com.jonrysimbolon.moviehiltmodular.R
+import com.jonrysimbolon.moviehiltmodular.databinding.DetailContentScrollingBinding
 import com.jonrysimbolon.moviehiltmodular.databinding.FragmentDetailBinding
+import com.jonrysimbolon.moviehiltmodular.utils.Constant
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,16 +15,43 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>(
 ) {
 
     override val baseViewModel: DetailViewModel by viewModels()
+    private lateinit var detContScroll: DetailContentScrollingBinding
 
     override fun setUpUi() {
         super.setUpUi()
+        detContScroll = DetailContentScrollingBinding.bind(binding.detContScroll.root)
 
-        /*val movieId = arguments?.getString(Constant.MOVIE_ID)
-        val movieTitle = arguments?.getString(Constant.MOVIE_TITLE)*/
+        val movieId = arguments?.getString(Constant.MOVIE_ID)
+        val movieTitle = arguments?.getString(Constant.MOVIE_TITLE)
 
         binding.apply {
-            //posterIv.transitionName = movieId
-            //requireActivity().title = movieTitle
+            toolbar.apply {
+                title = movieTitle
+                setNavigationOnClickListener {
+                    findNavController().popBackStack()
+                }
+                setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.reviewMenu -> {
+                            baseViewModel.showToast(getString(R.string.review_label))
+                            true
+                        }
+
+                        R.id.trailerMenu -> {
+                            baseViewModel.showToast(getString(R.string.trailer_label))
+                            true
+                        }
+
+                        else -> {
+                            false
+                        }
+                    }
+                }
+            }
+        }
+
+        detContScroll.apply {
+            posterIv.transitionName = movieId
         }
 
     }
@@ -29,7 +60,11 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>(
         super.setUpVm()
 
         binding.apply {
-            
+
+        }
+
+        detContScroll.apply {
+
         }
     }
 }
