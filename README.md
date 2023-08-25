e647ef365b0df51eea067c3280bf4663ca57737f
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-08-25 11:54:05 +0700

    docs: warn on readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MovieHiltModular
 Movie API + Modularization + Hilt
+
+nb:
+1. pada bagian detail, smooth scroll bagian title akan terjadi ketika di scroll (itu hal biasa)
+    akan tetapi yang perlu di warn adalah, itu hanya bisa dipakai pada `night mode`
--- app/src/main/java/com/jonrysimbolon/moviehiltmodular/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/jonrysimbolon/moviehiltmodular/presentation/detail/DetailFragment.kt
@@ -3,7 +3,6 @@ package com.jonrysimbolon.moviehiltmodular.presentation.detail
 import androidx.fragment.app.viewModels
 import com.jonrysimbolon.core.base.BaseFragment
 import com.jonrysimbolon.moviehiltmodular.databinding.FragmentDetailBinding
-import com.jonrysimbolon.moviehiltmodular.utils.Constant
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,17 +15,21 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>(
     override fun setUpUi() {
         super.setUpUi()
 
-        val movieId = arguments?.getString(Constant.MOVIE_ID)
-        val movieTitle = arguments?.getString(Constant.MOVIE_TITLE)
+        /*val movieId = arguments?.getString(Constant.MOVIE_ID)
+        val movieTitle = arguments?.getString(Constant.MOVIE_TITLE)*/
 
         binding.apply {
-            posterIv.transitionName = movieId
+            //posterIv.transitionName = movieId
             //requireActivity().title = movieTitle
         }
+
     }
 
     override fun setUpVm() {
         super.setUpVm()
 
+        binding.apply {
+            
+        }
     }
 }
